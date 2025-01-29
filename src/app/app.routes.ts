b0b57be20c965bd67c67317b65242c709b1e1144
Alan Lugo <pruebas@correo.com>
2025-01-29 13:16:16 -0600

    Se corrigio la ruta con load component en routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from '/home/champy/Escritorio/sucur/src/app/login/login.component';
 import { FinalComponent } from './final/final.component';
 import { PruebaComponent } from './prueba/prueba.component';
 import { BotonesComponent } from './botones/botones.component';
@@ -7,7 +6,8 @@ import { CompraTiempoAireComponent } from './tdd/compra-tiempo-aire/compra-tiemp
 import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
-    { path: '', component: LoginComponent },
+    { path: 'login',
+        loadComponent: () => import('./login/login.component').then(m => m.LoginComponent)},
     { path: 'final', component: FinalComponent },
     {path: 'prueba', component: PruebaComponent},
     {path: 'botones', component: BotonesComponent},
